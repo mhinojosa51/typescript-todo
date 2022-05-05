6db52f4dd91c0227c6587332878439b2111e5ce0
Michael Hinojosa <mikhinojosa@gmail.com>
2022-05-05 11:12:09 -0700

    added a new list component

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -2,17 +2,11 @@ import { render } from "@testing-library/react";
 import { FunctionComponent, useCallback, useEffect, useState } from "react";
 import { URLS } from "../../enums/urls";
 import "./Dashboard.scss";
-
-interface Todo {
-    userId: number;
-    id: number;
-    title: string;
-    completed: boolean;
-}
+import { Todo } from "../types";
+import { ListItem } from "../ListItem";
 
 export const Dashboard: FunctionComponent = (): JSX.Element => {
     const [todos, setTodos] = useState<Todo[]>([]);
-    const [filteredTodos, setFilteredTodos] = useState<Todo[]>([]);
     const [searchText, setSearchText] = useState<string>("");
 
     useEffect(() => {
@@ -21,17 +15,14 @@ export const Dashboard: FunctionComponent = (): JSX.Element => {
             const data: Todo[] = await res.json();
 
             setTodos(data);
-            setFilteredTodos(data);
         }
 
         fetchData();
     }, []);
 
-    const searchTodos = () => {
+    const searchTodos = (todos: Todo[]) => {
         const searchParam = searchText;
-        setFilteredTodos(
-            todos.filter((todo: Todo) => todo.title.includes(searchParam))
-        );
+        return todos.filter((todo: Todo) => todo.title.includes(searchParam));
     };
 
     const deleteToDo = (id: number) => {
@@ -41,14 +32,14 @@ export const Dashboard: FunctionComponent = (): JSX.Element => {
     const memoizedDeleteToDo = useCallback(deleteToDo, []);
 
     const renderTodoList = (): JSX.Element[] => {
-        return filteredTodos.map((todo: Todo) => {
+        return searchTodos(todos).map((todo: Todo) => {
             return (
-                <div
+                <ListItem
+                    onClick={() => deleteToDo(todo.id)}
+                    id={todo.id}
+                    title={todo.title}
                     key={todo.id}
-                    className='todo-item'
-                    onClick={() => memoizedDeleteToDo(todo.id)}>
-                    {todo.title}
-                </div>
+                />
             );
         });
     };
@@ -62,9 +53,7 @@ export const Dashboard: FunctionComponent = (): JSX.Element => {
                     type='text'
                     onChange={(ev) => setSearchText(ev.currentTarget.value)}
                 />
-                <button className='search-button' onClick={searchTodos}>
-                    Submit
-                </button>
+                {/* <button className='search-button'>Submit</button> */}
             </section>
             {renderTodoList()}
         </div>
--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,4 +1,20 @@
 import { FunctionComponent } from "react";
 import "./ListItem.scss";
 
-export const ListItem = () => null;
+interface ListItemIProps {
+    id: number;
+    title: string;
+    onClick: () => void;
+}
+
+export const ListItem: FunctionComponent<ListItemIProps> = ({
+    id,
+    title,
+    onClick,
+}): JSX.Element => {
+    return (
+        <div className='todo-item' onClick={onClick}>
+            {title}
+        </div>
+    );
+};
